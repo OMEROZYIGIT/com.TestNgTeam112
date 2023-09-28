45114b76687e86ebe5623ac870ee5a4334e9db53
OMEROZYIGIT <ozyigitomer9@gmail.com>
2023-09-28 23:48:25 +0300

    crossDriver and testbasecross

--- src/test/java/utilities/CrossDriver.java
+++ src/test/java/utilities/CrossDriver.java
@@ -1,2 +1,51 @@
-package utilities;public class CrossDriver {
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CrossDriver {
+
+    private CrossDriver(){
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver (String browser){
+
+        browser = browser==null ?  ConfigReader.getProperty("browser"):browser;
+
+        if (driver==null){
+
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+            }
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        if (driver!=null){
+            driver.quit();
+            driver=null;
+        }
+    }
 }
--- src/test/java/utilities/TestBaseCross.java
+++ src/test/java/utilities/TestBaseCross.java
@@ -1,2 +1,31 @@
-package utilities;public class TestBaseCross {
+package utilities;
+
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
+public class TestBaseCross {
+
+    protected WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeMethod
+    public void setUp(@Optional String browser){
+
+        driver = CrossDriver.getDriver(browser);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @AfterMethod
+    public void tearDown (){
+
+        CrossDriver.closeDriver();
+    }
 }
